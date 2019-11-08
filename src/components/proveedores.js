322969389ddfb254a5f2ce6fc4419ce7bf653bcd
Maira <mairatorres.2110@gmail.com>
2019-11-08 10:27:19 -0300

    creacion de formulario proveedor

---
+++ src/components/proveedores.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Menu from './components/barra';
+
+function proveedores() {
+  return (
+    <div className="proveedores">
+        <Menu /> 
+        <nav className = "navbar navbar-dark bg-dark">
+         <a href="" className ="text-white">
+             task
+         </a>
+         
+
+        </nav>
+  
+   
+    </div>
+);
+}
+export default proveedores;
